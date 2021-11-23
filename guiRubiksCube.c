10615d09f4c242b48cecd576f88ebbe59c35ba3f
cyb3rDr3am <nairod62300@outlook.fr>
2021-11-23 19:50:41 +0100

    modifié :         guiRubiksCube.c 	modifié :         guiRubiksCube.h

--- guiRubiksCube.c
+++ guiRubiksCube.c
@@ -1,11 +1,14 @@
 #include "guiRubiksCube.h"
 #include "rubiksCube.h"
-void init(Cube *cube) {
+void init() {
     SDL_Window *window;
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_LOG("erreur initialisation SDL > %s\n",SDL_GetError());
+        SDL_Log("erreur initialisation SDL > %s\n",SDL_GetError());
         exit(EXIT_FAILURE);
     }
-    window = SDL_CreateWindow("Rubiks cube",0,0,SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH_WINDOW,HEIGHT_WINDOW);
+    window = SDL_CreateWindow("Rubiks cube",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH_WINDOW,HEIGHT_WINDOW,0);
     SDL_Quit();
+}
+int main(void) {
+    init();
 }
